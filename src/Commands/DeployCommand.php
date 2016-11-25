c0c1aa31d66f572e8cfc4294d2d48c0722cb1a5b
Rahul Kadyan <hi@znck.me>
2016-11-25 16:23:03 +0530

    :construction: Display status and exit on failure in deploy command

--- src/Commands/DeployCommand.php
+++ src/Commands/DeployCommand.php
@@ -24,28 +24,44 @@ class DeployCommand extends Command
         $upload = new Process('git push -u origin');
         $publish = new Process('git subtree push --prefix public origin '.config('github.branch'));
 
+        $output->writeln('=> '.$save->getCommandLine());
         $save->run();
         $output->write($save->getOutput());
+        if (!$save->isSuccessful()) {
+            $this->dd($output, $save);
+        }
+
         $upload->run();
         $output->write($upload->getOutput());
+        if (!$upload->isSuccessful()) {
+            $this->dd($output, $upload);
+        }
+
         $publish->run();
         $output->write($publish->getOutput());
 
         if ($publish->isSuccessful()) {
             $output->writeln('<info>Site was generated successfully.</info>');
         } else {
-            $output->writeln('<error>There was some error.</error>'.PHP_EOL.$publish->getErrorOutput());
+            $this->dd($output, $publish);
         }
+    }
+
+    protected function dd(OutputInterface $output, Process $process) {
+        $output->writeln('<error>There was some error.</error>'.PHP_EOL.$process->getErrorOutput());
+        exit($process->getExitCode());
     }
 
     protected function build(OutputInterface $output)
     {
+        $output->writeln('Building website...');
         $gulp = new Process('./node_modules/.bin/gulp');
         app(Filesystem::class)->cleanDirectory(cache_dir());
         $gulp->run();
         if (! $gulp->isSuccessful()) {
             $output->writeln('<error>Javascript dependencies not installed</error>');
             $output->writeln('Run <info>yarn</info> or <info>npm install</info>');
+            exit($gulp->getExitCode());
         }
     }
 }
