76d71428476a1ded31541f65a1e643f236a92be3
Rahul Kadyan <hi@znck.me>
2016-12-04 18:04:37 +0530

    :bug: Use filename to build blade pages

--- src/Processors/BladeProcessor.php
+++ src/Processors/BladeProcessor.php
@@ -20,13 +20,12 @@ class BladeProcessor extends AbstractProcessor
 
     public function process(SplFileInfo $file, array $data, array $options = [])
     {
-        $path = $this->getPath($file);
         $filename = $this->getOutputFilename($file, array_get($options, 'interceptor'));
 
         $data['currentViewPath'] = $path;
         $data['currentUrlPath'] = $this->getUrl($filename);
 
-        $content = $this->viewFactory->make($path, $data)->render();
+        $content = $this->viewFactory->file($file->getPathname(), $data)->render();
 
         app()->line('Blade: '.$file->getRelativePathname().' -> '.$filename);
 
